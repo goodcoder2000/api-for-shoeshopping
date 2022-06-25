abeb73f45527f62df73848709bad45b507749ff5
goodcoder2000 <goodcoder2000@gmail.com>
2022-06-25 20:24:14 +0630

    final api

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
 const app = express();
 const mongoClient = require("mongodb").MongoClient;
 const { ObjectId } = require("mongodb");
 
 app.use(express.json());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }));
 
-app.use(cors(
-    {
-        "origin":   "*"
-    }
-))
-
-app.listen(process.env.PORT || 3001, () =>{
-    console.log("Server is running at 3001");
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('server is running')
 })
 
 let db;
@@ -37,14 +37,11 @@ app.get('/users', (req, res) =>{
     
 })
 // get single docs
-app.get('users/:id', (req, res) =>{
-    const useId = req.params.id;
-    db.collection('users').findOne({_id: ObjectId(useId)})
+app.get('/users/:id', (req, res) =>{
+    const userId = req.params.id;
+    db.collection("users").findOne({_id: ObjectId(userId)})
     .then((result) =>{
-        if(err){
-            res.status(500).json({err: err})     
-        }   
-        res.status(200).json(result);
+        res.status(200).json(result)
     })
 })
 
@@ -53,30 +50,27 @@ app.post('/users', (req, res) =>{
     const data = req.body;
     db.collection('users').insertOne(data)
     .then((result) =>{
-        if(err){
-            res.status(500).json({err: err})     
-        }
-        res.status(201).json(result)
+        res.status(201).json(result);
     })
 })
 
 
 // for nesked docs
-app.patch('users/:id/:method', (req,res) =>{
+app.patch("/users/:id/:method", (req, res) =>{
     const userId = req.params.id;
     const method = req.params.method;
     const data = req.body;
-     if(method ==="pull"){
-        db.collection('users').updateOne({_id: ObjectId(userId)}, {$pull: {orderpost: data}})
+    if("push" === method){
+        db.collection("users").updateOne({_id: ObjectId(userId)}, {$addToSet: {orderpost: data}})
         .then((result) =>{
             res.status(201).json(result);
         })
-        } else if(method ==="push"){
-            db.collection('users').updateOne({_id: ObjectId(userId)}, {$addToSet: {orderpost: data}})
-            .then((result) =>{
-                res.status(201).json(result);
-            })
-        }
+    } else if("pull" === method){
+        db.collection("users").updateOne({_id: ObjectId(userId)}, {$pull: {orderpost: data}})
+        .then((result) =>{
+            res.status(201).json(result);
+        })
+    }
 })
 
 // getting all shoesmenu
@@ -92,13 +86,10 @@ app.get('/shoesmenu', (req, res) =>{
     
 })
 // get single docs
-app.get('shoesmenu/:id', (req, res) =>{
-    const useId = req.params.id;
-    db.collection('shoesmenu').findOne({_id: ObjectId(useId)})
+app.get('/shoesmenu/:id', async (req, res) =>{
+    const id = req.params.id;
+    await db.collection('shoesmenu').findOne({_id: ObjectId(id)})
     .then((result) =>{
-        if(err){
-            res.status(500).json({err: err})     
-        }
         res.status(200).json(result);
     })
 })
@@ -108,9 +99,6 @@ app.post('/shoesmenu', (req, res) =>{
     const data = req.body;
     db.collection('shoesmenu').insertOne(data)
     .then((result) =>{
-        if(err){
-            res.status(500).json({err: err})     
-        }
         res.status(201).json(result)
     })
 })
